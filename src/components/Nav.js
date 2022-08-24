43e26dbe7f805250f64b508069d059b4e4fd868b
Damian Martin <dmartinr2d2@gmail.com>
2022-08-24 12:52:11 +0100

    this commit will add material ui styling for links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,35 @@
 import { Link } from 'react-router-dom';
+import { Button, Typography } from '@mui/material';
+import { Home, SportsSoccer, OutdoorGrill, Code } from '@mui/icons-material';
 
 const Nav = () => {
   return (
     <div className="navbar">
       <div>
-        <h2>Paddington Post</h2>
+        <Typography className="main_title" variant="h2" component="h2">
+          Paddington Post
+        </Typography>
       </div>
       <div className="nav_all_link">
-        <Link className="nav_link" id="home" to="/">
-          Home
+        <Link to="/">
+          <Button variant="contained" color="success">
+            <Home className="icon" /> Home
+          </Button>
         </Link>
-        <Link className="nav_link" to="/articles/topics/coding">
-          Coding
+        <Link to="/articles/topics/coding">
+          <Button variant="outlined" color="warning">
+            <Code className="icon" /> Coding
+          </Button>
         </Link>
-        <Link className="nav_link" to="/articles/topics/cooking">
-          Cooking
+        <Link to="/articles/topics/cooking">
+          <Button variant="outlined" color="warning">
+            <OutdoorGrill className="icon" /> Cooking
+          </Button>
         </Link>
-        <Link className="nav_link" to="/articles/topics/football">
-          Football
+        <Link to="/articles/topics/football">
+          <Button variant="outlined" color="warning">
+            <SportsSoccer className="icon" /> Football
+          </Button>
         </Link>
       </div>
     </div>
