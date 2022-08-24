3f12471b3f1874042afe99ff02ad61c9dfa8c399
Damian Martin <dmartinr2d2@gmail.com>
2022-08-24 12:53:17 +0100

    this commit will add material ui styling components and votes icon

--- src/components/SingleArticleCard.js
+++ src/components/SingleArticleCard.js
@@ -1,42 +1,68 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Card, CardContent, CardMedia, Typography } from '@mui/material';
+import { FavoriteRounded } from '@mui/icons-material';
 
 const SingleArticleCard = ({ article }) => {
   const date = article.created_at;
   const displayDate = new Date(date).toLocaleString('en-GB');
 
   return (
-    <li className="article_card">
-      <div className="article_image">
-        <img
-          src="https://images.unsplash.com/photo-1612178537253-bccd437b730e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-          alt={article.title}
-        />
-        <div className="topic-icon">
-          <p className="article_info__topic">{article.topic}</p>
-        </div>
-      </div>
-      <div className="article_info">
-        <p className="article_info__date">{displayDate}</p>
+    <Card>
+      <CardMedia
+        component="img"
+        height="140"
+        image="https://source.unsplash.com/random"
+        alt="random"
+      />
+      <CardContent>
+        <Typography
+          gutterBottom
+          variant="p"
+          component="p"
+          style={{ fontSize: 10, paddingBottom: 10, color: '#ED6C02' }}>
+          {article.topic}
+        </Typography>
+        <Typography
+          color="text.secondary"
+          gutterBottom
+          variant="p"
+          component="p"
+          style={{ fontSize: 12 }}>
+          {displayDate}
+        </Typography>
         <Link
+          className="card_title"
+          style={{
+            textDecoration: 'none',
+            color: 'inherit'
+          }}
           to={`/articles/topics/${article.topic}/article-${article.article_id}`}>
-          <p className="article_info__title">{article.title}</p>
+          <Typography
+            className="card_title"
+            style={{ padding: 20 }}
+            gutterBottom
+            variant="h6"
+            component="h4"
+            sx={{ height: 115 }}>
+            {article.title}
+          </Typography>
         </Link>
-        <div className="article_footer">
-          <div className="article_info__author">
-            <p>Written by - {article.author}</p>
-          </div>
-          <div className="article_info__stats">
-            <p className="article_info__comment-count">
-              <span className="counts">{article.comment_count}</span> comments
-            </p>
-            <p className="article_info__votes">
-              <span className="counts">{article.votes}</span> votes
-            </p>
-          </div>
-        </div>
-      </div>
-    </li>
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          style={{ fontSize: 12 }}>
+          Written by - {article.author}
+        </Typography>
+        <Typography
+          variant="body2"
+          alignItems={'center'}
+          style={{ fontSize: 12, marginTop: 10 }}>
+          {article.votes}
+        </Typography>
+        <FavoriteRounded color="error" fontSize="small" />
+      </CardContent>
+    </Card>
   );
 };
 
