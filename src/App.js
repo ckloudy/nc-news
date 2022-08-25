0d1e9af0fff45f6f98de7da7bcc552310b099f53
Damian Martin <dmartinr2d2@gmail.com>
2022-08-25 15:42:40 +0100

    this commit removes unused component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ArticleList from './components/ArticleList';
-import LoggedInUser from './components/LoggedInUser';
 import Nav from './components/Nav';
 import FilterArticles from './components/FilterArticles';
 import SingleFullArticle from './components/SingleFullArticle';
+import { UserContext } from './contexts/UserContext';
+import { useState } from 'react';
 
 function App() {
+  const [ user, setUser ] = useState({
+    name: 'Jonny',
+    username: 'jessjelly'
+  });
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Nav />
-        {/* <LoggedInUser /> */}
-        <Routes>
-          <Route path="/" element={<ArticleList />} />
-          <Route path="/articles/topics/:topic" element={<FilterArticles />} />
-          <Route
-            path="/articles/topics/:topic/article-:id"
-            element={<SingleFullArticle />}
-          />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <UserContext.Provider value={{ user, setUser }}>
+      <BrowserRouter>
+        <div className="App">
+          <Nav />
+          <Routes>
+            <Route path="/" element={<ArticleList />} />
+            <Route
+              path="/articles/topics/:topic"
+              element={<FilterArticles />}
+            />
+            <Route
+              path="/articles/topics/:topic/article-:id"
+              element={<SingleFullArticle />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
