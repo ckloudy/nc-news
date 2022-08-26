3c76c5f22d5373c178b4cba1bcd01545d9bcc8e7
Damian Martin <dmartinr2d2@gmail.com>
2022-08-26 12:12:13 +0100

    this commit refactors api calls for articles

--- src/utils/articlesApi.js
+++ src/utils/articlesApi.js
@@ -1,47 +1,17 @@
-export function getArticles() {
-  return fetch('https://dnc-news.herokuapp.com/api/articles').then((res) => {
-    return res.json();
-  });
-}
+import axios from 'axios';
 
-export function getArticlesSort(topic, sort) {
-  return fetch(
-    `https://dnc-news.herokuapp.com/api/articles?topic=${topic}&sort_by=${sort}`
-  ).then((res) => {
-    return res.json();
-  });
-}
-
-export function getArticlesSortNoTopic(sort) {
-  return fetch(
-    `https://dnc-news.herokuapp.com/api/articles?sort_by=${sort}`
-  ).then((res) => {
-    return res.json();
-  });
-}
-
-export function getArticlesOrder(topic, order) {
-  return fetch(
-    `https://dnc-news.herokuapp.com/api/articles?topic=${topic}&order=${order}`
-  ).then((res) => {
-    return res.json();
-  });
-}
-
-export function getArticlesOrderNoTopic(order) {
-  return fetch(
-    `https://dnc-news.herokuapp.com/api/articles?order=${order}`
-  ).then((res) => {
-    return res.json();
-  });
-}
-
-export function getArticlesByTopic(topic) {
-  return fetch(
-    `https://dnc-news.herokuapp.com/api/articles?topic=${topic}`
-  ).then((res) => {
-    return res.json();
-  });
+export function getArticles(topic, sort = 'created_at', order = 'DESC') {
+  return axios
+    .get(`https://dnc-news.herokuapp.com/api/articles`, {
+      params: {
+        topic: topic,
+        sort_by: sort,
+        order: order
+      }
+    })
+    .then(({ data }) => {
+      return data;
+    });
 }
 
 export function getFullArticle(id) {
