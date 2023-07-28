802338f0d56da8d15f530eb56393b4027936cba8
Damian Martin <dmartinr2d2@gmail.com>
2023-07-28 09:28:01 +0100

    add styling

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,124 +1,121 @@
-import { useState, useEffect } from 'react';
-import { getArticles } from '../utils/articlesApi';
-import SingleArticleCard from './SingleArticleCard';
+import { useState, useEffect } from "react";
+import { getArticles } from "../utils/articlesApi";
+import SingleArticleCard from "./SingleArticleCard";
 import {
-  Grid,
-  Container,
-  Typography,
-  Box,
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem
-} from '@mui/material';
-import { UserContext } from '../contexts/UserContext';
-import { useContext } from 'react';
-import { capitalizeFirstLetter } from '../utils/Capitalize';
+    Grid,
+    Container,
+    Typography,
+    Box,
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem,
+} from "@mui/material";
+import { UserContext } from "../contexts/UserContext";
+import { useContext } from "react";
+import { capitalizeFirstLetter } from "../utils/Capitalize";
 
 const ArticleList = ({ topic }) => {
-  const [ articles, setArticles ] = useState([]);
-  const [ isLoading, setIsLoading ] = useState(true);
-  const [ topicCap, setTopicCap ] = useState('');
-  const [ sort, setSort ] = useState('created_at');
-  const [ order, setOrder ] = useState('DESC');
-  const { user } = useContext(UserContext);
+    const [articles, setArticles] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [topicCap, setTopicCap] = useState("");
+    const [sort, setSort] = useState("created_at");
+    const [order, setOrder] = useState("DESC");
+    const { user } = useContext(UserContext);
 
-  useEffect(
-    () => {
-      getArticles(topic, sort, order)
-        .then((articlesFromApi) => {
-          setArticles(articlesFromApi.articles);
-          setIsLoading(false);
-          const cap = capitalizeFirstLetter(topic);
-          setTopicCap(cap);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },
-    [ topic, isLoading, sort, order ]
-  );
+    useEffect(() => {
+        getArticles(topic, sort, order)
+            .then((articlesFromApi) => {
+                setArticles(articlesFromApi.articles);
+                setIsLoading(false);
+                const cap = capitalizeFirstLetter(topic);
+                setTopicCap(cap);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, [topic, isLoading, sort, order]);
 
-  const handleChange = (e) => {
-    setSort(e.target.value);
-  };
+    const handleChange = (e) => {
+        setSort(e.target.value);
+    };
 
-  const handleOrder = (e) => {
-    setOrder(e.target.value);
-  };
+    const handleOrder = (e) => {
+        setOrder(e.target.value);
+    };
 
-  if (isLoading) {
-    return (
-      <div className="loading">
-        <p>Loading...</p>
-      </div>
-    );
-  } else {
-    return (
-      <Container style={{ marginBottom: 50 }}>
-        <Typography variant="h5">Hello, {user.name}</Typography>
-        <Container
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
-          }}>
-          <Typography
-            variant="h4"
-            component="h2"
-            style={{ padding: 40, color: '#363534' }}>
-            {topicCap} Articles
-          </Typography>
-          <Box sx={{ minWidth: 80, width: 270, mr: 5, display: 'flex' }}>
-            <FormControl fullWidth size="small">
-              <InputLabel color="success" id="select-label">
-                Sort By
-              </InputLabel>
-              <Select
-                color="success"
-                labelId="select-label-sort"
-                id="simple-select"
-                label="Sort By"
-                value={sort}
-                onChange={handleChange}>
-                <MenuItem value={'created_at'}>Most recent</MenuItem>
-                <MenuItem value={'article_id'}>id</MenuItem>
-                <MenuItem value={'title'}>Title</MenuItem>
-                <MenuItem value={'author'}>Author</MenuItem>
-                <MenuItem value={'votes'}>Votes</MenuItem>
-                <MenuItem value={'comment_count'}>Comments</MenuItem>
-              </Select>
-            </FormControl>
-            <FormControl fullWidth size="small">
-              <InputLabel color="success" id="select-label">
-                Order
-              </InputLabel>
-              <Select
-                color="success"
-                labelId="select-label-order"
-                id="simple-select"
-                label="Order"
-                value={order}
-                onChange={handleOrder}>
-                <MenuItem value={'ASC'}>Asc</MenuItem>
-                <MenuItem value={'DESC'}>Desc</MenuItem>
-              </Select>
-            </FormControl>
-          </Box>
-        </Container>
-        <Grid container spacing={4}>
-          {articles.map((article) => {
-            return (
-              <Grid item key={article.article_id} xs={12} sm={6} md={4}>
-                <SingleArticleCard article={article} />
-              </Grid>
-            );
-          })}
-        </Grid>
-      </Container>
-      // </div>
-    );
-  }
+    if (isLoading) {
+        return (
+            <div className="loading">
+                <p>Loading...</p>
+            </div>
+        );
+    } else {
+        return (
+            <Container style={{ marginBottom: 50 }}>
+                <Typography variant="h5">Hello, {user.name}</Typography>
+                <Container
+                    sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}>
+                    <Typography
+                        variant="h4"
+                        component="h2"
+                        style={{ padding: 40, color: "#363534" }}>
+                        {topicCap} Articles
+                    </Typography>
+                    <Box sx={{ minWidth: 80, width: 270, mr: 5, display: "flex" }}>
+                        <FormControl fullWidth size="small">
+                            <InputLabel color="success" id="select-label">
+                                Sort By
+                            </InputLabel>
+                            <Select
+                                color="success"
+                                labelId="select-label-sort"
+                                id="simple-select"
+                                label="Sort By"
+                                value={sort}
+                                onChange={handleChange}>
+                                <MenuItem value={"created_at"}>Most recent</MenuItem>
+                                <MenuItem value={"article_id"}>id</MenuItem>
+                                <MenuItem value={"title"}>Title</MenuItem>
+                                <MenuItem value={"author"}>Author</MenuItem>
+                                <MenuItem value={"votes"}>Votes</MenuItem>
+                                <MenuItem value={"comment_count"}>Comments</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <FormControl fullWidth size="small">
+                            <InputLabel color="success" id="select-label">
+                                Order
+                            </InputLabel>
+                            <Select
+                                color="success"
+                                labelId="select-label-order"
+                                id="simple-select"
+                                label="Order"
+                                value={order}
+                                onChange={handleOrder}>
+                                <MenuItem value={"ASC"}>Asc</MenuItem>
+                                <MenuItem value={"DESC"}>Desc</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Box>
+                </Container>
+                <Grid container spacing={4}>
+                    {articles.map((article, index) => {
+                        return (
+                            <Grid item key={article.article_id} xs={12} sm={6} md={4}>
+                                <SingleArticleCard article={article} index={index} />
+                            </Grid>
+                        );
+                    })}
+                </Grid>
+            </Container>
+            // </div>
+        );
+    }
 };
 
 export default ArticleList;
--- src/components/SingleArticleCard.js
+++ src/components/SingleArticleCard.js
@@ -1,88 +1,80 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Card, CardContent, CardMedia, Typography, Box } from '@mui/material';
-import { FavoriteRounded, QuestionAnswer } from '@mui/icons-material';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Card, CardContent, CardMedia, Typography, Box } from "@mui/material";
+import { FavoriteRounded, QuestionAnswer } from "@mui/icons-material";
 
-const SingleArticleCard = ({ article }) => {
-  const date = article.created_at;
-  const displayDate = new Date(date).toLocaleString('en-GB');
+const SingleArticleCard = ({ article, index }) => {
+    const date = article.created_at;
+    const displayDate = new Date(date).toLocaleString("en-GB");
 
-  return (
-    <Card>
-      <CardMedia
-        component="img"
-        height="140"
-        image="https://source.unsplash.com/random"
-        alt="random"
-      />
-      <CardContent>
-        <Typography
-          gutterBottom
-          variant="p"
-          component="p"
-          style={{ fontSize: 10, paddingBottom: 10, color: '#ED6C02' }}>
-          {article.topic}
-        </Typography>
-        <Typography
-          color="text.secondary"
-          gutterBottom
-          variant="p"
-          component="p"
-          style={{ fontSize: 12 }}>
-          {displayDate}
-        </Typography>
-        <Link
-          className="card_title"
-          style={{
-            textDecoration: 'none',
-            color: 'inherit'
-          }}
-          to={`/articles/topics/${article.topic}/article-${article.article_id}`}>
-          <Typography
-            className="card_title"
-            style={{ padding: 20 }}
-            gutterBottom
-            variant="h6"
-            component="h4"
-            sx={{ height: 115 }}>
-            {article.title}
-          </Typography>
-        </Link>
-        <Typography
-          variant="body2"
-          color="text.secondary"
-          style={{ fontSize: 12 }}>
-          Written by - {article.author}
-        </Typography>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginTop: 3
-          }}>
-          <Typography
-            variant="body2"
-            alignItems={'center'}
-            style={{ fontSize: 15, marginTop: 10, marginRight: 6 }}>
-            {article.comment_count}
-          </Typography>
-          <QuestionAnswer
-            color="primary"
-            fontSize="small"
-            sx={{ marginRight: 10 }}
-          />
-          <Typography
-            variant="body2"
-            alignItems={'center'}
-            style={{ fontSize: 15, marginTop: 10, marginRight: 6 }}>
-            {article.votes}
-          </Typography>
-          <FavoriteRounded color="error" fontSize="small" />
-        </Box>
-      </CardContent>
-    </Card>
-  );
+    return (
+        <Card>
+            <CardMedia
+                component="img"
+                height="250"
+                image={`https://source.unsplash.com/featured/${index}`}
+                alt="random"
+            />
+            <CardContent>
+                <Typography
+                    gutterBottom
+                    variant="p"
+                    component="p"
+                    style={{ fontSize: 10, paddingBottom: 10, color: "#ED6C02" }}>
+                    {article.topic}
+                </Typography>
+                <Typography
+                    color="text.secondary"
+                    gutterBottom
+                    variant="p"
+                    component="p"
+                    style={{ fontSize: 12 }}>
+                    {displayDate}
+                </Typography>
+                <Link
+                    className="card_title"
+                    style={{
+                        textDecoration: "none",
+                        color: "inherit",
+                    }}
+                    to={`/articles/topics/${article.topic}/article-${article.article_id}`}>
+                    <Typography
+                        className="card_title"
+                        style={{ padding: 20 }}
+                        gutterBottom
+                        variant="h6"
+                        component="h4"
+                        sx={{ height: 115 }}>
+                        {article.title}
+                    </Typography>
+                </Link>
+                <Typography variant="body2" color="text.secondary" style={{ fontSize: 12 }}>
+                    Written by - {article.author}
+                </Typography>
+                <Box
+                    sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}>
+                    <Typography
+                        variant="body2"
+                        alignItems={"center"}
+                        style={{ fontSize: 15, marginTop: 10, marginRight: 6 }}>
+                        {article.comment_count}
+                    </Typography>
+                    <QuestionAnswer color="primary" fontSize="small" sx={{ marginRight: 10 }} />
+                    <Typography
+                        variant="body2"
+                        alignItems={"center"}
+                        style={{ fontSize: 15, marginTop: 10, marginRight: 6 }}>
+                        {article.votes}
+                    </Typography>
+                    <FavoriteRounded color="error" fontSize="small" />
+                </Box>
+            </CardContent>
+        </Card>
+    );
 };
 
 export default SingleArticleCard;
