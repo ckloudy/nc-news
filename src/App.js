ac7ac1dbf07a01322f266ba5ec47707ab3468b0a
Damian Martin <dmartinr2d2@gmail.com>
2023-07-30 00:58:02 +0100

    adjust title sizing

--- src/App.js
+++ src/App.js
@@ -14,34 +14,32 @@ function App() {
         username: "jessjelly",
     });
     return (
-        <div className="">
-            <UserContext.Provider value={{ user, setUser }}>
-                <BrowserRouter>
-                    <div id="outer-container">
-                        <div className="container mx-auto" id="page-wrap">
-                            <Nav user={user} />
-                            <Sidebar user={user} />
-                        </div>
+        <UserContext.Provider value={{ user, setUser }}>
+            <BrowserRouter>
+                <div id="outer-container">
+                    <div className="container mx-auto" id="page-wrap">
+                        <Nav user={user} />
+                        <Sidebar user={user} />
                     </div>
+                </div>
 
-                    <div className=" w-full text-center my-16 font-sans">
-                        <div className="]">
-                            <p className="text-3xl md:text-5xl font-black text-white drop-shadow-sm bg-gradient-to-r from-cyan-500 to-blue-500 p-10">
-                                Get all the latest and trending news
-                            </p>
-                        </div>
+                <div className=" w-full text-center my-8 font-sans">
+                    <div className="]">
+                        <p className="text-3xl md:text-5xl font-black text-white drop-shadow-sm bg-gradient-to-r from-cyan-500 to-blue-500 p-7">
+                            Get all the latest and trending news
+                        </p>
                     </div>
-                    <Routes>
-                        <Route path="/" element={<ArticleList />} />
-                        <Route path="/articles/topics/:topic" element={<FilterArticles />} />
-                        <Route
-                            path="/articles/topics/:topic/article-:id"
-                            element={<SingleFullArticle />}
-                        />
-                    </Routes>
-                </BrowserRouter>
-            </UserContext.Provider>
-        </div>
+                </div>
+                <Routes>
+                    <Route path="/" element={<ArticleList />} />
+                    <Route path="/articles/topics/:topic" element={<FilterArticles />} />
+                    <Route
+                        path="/articles/topics/:topic/article-:id"
+                        element={<SingleFullArticle />}
+                    />
+                </Routes>
+            </BrowserRouter>
+        </UserContext.Provider>
     );
 }
 
